e02ba2bd7b691d2481ac0d90d7563e24ed1ece51
Kenji Saito <ken-yo@mbr.nifty.com>
2019-08-17 21:30:15 +0900

    Fix the build error and Bump to openjdk11

--- src/test/java/com/github/poad/functional/type/tuple/PairTest.java
+++ src/test/java/com/github/poad/functional/type/tuple/PairTest.java
@@ -9,37 +9,30 @@ import org.junit.runner.RunWith;
 import java.util.Collections;
 import java.util.List;
 
-@RunWith(Enclosed.class)
-public class PairTest {
+public class PairTest extends ScenarioTest<PairTestStep, PairTestStep, PairTestStep> {
 
-    public static class IntegerTest  extends ScenarioTest<PairTestStep, PairTestStep, PairTestStep> {
-        @Test
-        public void normal() {
-            given().generateInstanceWith(1, 2);
-            then().pairWith(1, 2)
-                    .and()
-                    .toString("(1, 2)");
-        }
+    @Test
+    public void inteegr_normal() {
+        given().generateInstanceWith(1, 2);
+        then().pairWith(1, 2)
+                .and()
+                .toString("(1, 2)");
     }
 
-    public static class ObjectTest  extends ScenarioTest<PairTestStep, PairTestStep, PairTestStep> {
-        @Test
-        public void normal() {
-            final List<Integer> list = Collections.singletonList(123);
-            given().generateInstanceWith("abc", list);
-            then().pairWith("abc", list)
-                    .and()
-                    .toString("(abc, " + list.toString() + ")");
-        }
+    @Test
+    public void object_normal() {
+        final List<Integer> list = Collections.singletonList(123);
+        given().generateInstanceWith("abc", list);
+        then().pairWith("abc", list)
+                .and()
+                .toString("(abc, " + list.toString() + ")");
     }
 
-    public static class ObjectWithNullTest  extends ScenarioTest<PairTestStep, PairTestStep, PairTestStep> {
-        @Test
-        public void normal() {
-            given().generateInstanceWith("abc", null);
-            then().pairWith("abc", null)
-                    .and()
-                    .toString("(abc, null)");
-        }
+    @Test
+    public void object_with_null_normal() {
+        given().generateInstanceWith("abc", null);
+        then().pairWith("abc", null)
+                .and()
+                .toString("(abc, null)");
     }
 }
