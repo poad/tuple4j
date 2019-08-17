98d0248b419f97bc3a61c7e98cd46816b651c996
Kenji Saito <ken-yo@mbr.nifty.com>
2019-08-17 22:03:38 +0900

    Fix the build error and Bump to openjdk11 (#1)

--- src/test/java/com/github/poad/functional/type/tuple/TripleTest.java
+++ src/test/java/com/github/poad/functional/type/tuple/TripleTest.java
@@ -1,45 +1,36 @@
 package com.github.poad.functional.type.tuple;
 
 import com.tngtech.jgiven.junit.ScenarioTest;
-import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
 import com.github.poad.functional.type.tuple.step.TripleTestStep;
+import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
 
-@RunWith(Enclosed.class)
-public class TripleTest {
+public class TripleTest  extends ScenarioTest<TripleTestStep, TripleTestStep, TripleTestStep> {
 
-    public static class IntegerTest  extends ScenarioTest<TripleTestStep, TripleTestStep, TripleTestStep> {
-        @Test
-        public void normal() {
-            given().generateInstanceWith(1, 2, 3);
-            then().tripleWith(1, 2, 3)
-                    .and()
-                    .toString("(1, 2, 3)");
-        }
+    @Test
+    public void integer_normal() {
+        given().generateInstanceWith(1, 2, 3);
+        then().tripleWith(1, 2, 3)
+                .and()
+                .toString("(1, 2, 3)");
     }
 
-    public static class ObjectTest  extends ScenarioTest<TripleTestStep, TripleTestStep, TripleTestStep> {
-        @Test
-        public void normal() {
-            final List<Integer> list = Collections.singletonList(123);
-            given().generateInstanceWith("abc", list, 123);
-            then().tripleWith("abc", list, 123)
-                    .and()
-                    .toString("(abc, " + list.toString() + ", 123)");
-        }
+    @Test
+    public void object_normal() {
+        final List<Integer> list = Collections.singletonList(123);
+        given().generateInstanceWith("abc", list, 123);
+        then().tripleWith("abc", list, 123)
+                .and()
+                .toString("(abc, " + list.toString() + ", 123)");
     }
 
-    public static class ObjectWithNullTest  extends ScenarioTest<TripleTestStep, TripleTestStep, TripleTestStep> {
-        @Test
-        public void normal() {
-            given().generateInstanceWith("abc", null, null);
-            then().tripleWith("abc", null, null)
-                    .and()
-                    .toString("(abc, null, null)");
-        }
+    @Test
+    public void object_with_null_normal() {
+        given().generateInstanceWith("abc", null, null);
+        then().tripleWith("abc", null, null)
+                .and()
+                .toString("(abc, null, null)");
     }
 }
